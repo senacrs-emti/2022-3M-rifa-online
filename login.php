ab187280966ec95be17a172679cc143b6fd672ee
Aninha <ana.wrab@gmail.com>
2022-05-09 08:58:54 -0300

    asdcad

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" >
+    <title> Login </title>
+</head>
+<body>
+    <div class="menu">
+        <div class="content">
+            <img src="https://engenharia360.com/wp-content/uploads/2019/05/esta-pessoa-nao-existe-engenharia360-4.png" alt="Foto Usuário" width="128" height="128">
+            <h1>Nome Usuário</h1>
+            <a href="">Editar Perfil</a>
+            <a href="">Rifas Atuais</a>
+            <a href="">Logout</a>
+        </div>
+    </div>
+
+    
+</div>
+</body>
+</html>
