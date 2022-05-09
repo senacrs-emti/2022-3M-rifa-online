16df1a3cb14bac68d82078f052c775fda71a836e
Aninha <ana.wrab@gmail.com>
2022-05-09 09:47:33 -0300

    login
    pagina de editar perfil e mostrar rifas atuais

--- login.php
+++ login.php
@@ -11,10 +11,10 @@
     <div class="menu">
         <div class="content">
             <img src="https://engenharia360.com/wp-content/uploads/2019/05/esta-pessoa-nao-existe-engenharia360-4.png" alt="Foto Usuário" width="128" height="128">
-            <h1>Nome Usuário</h1>
-            <a href="">Editar Perfil</a>
-            <a href="">Rifas Atuais</a>
-            <a href="">Logout</a>
+            <h1>Nome Usuário</h1><br>
+            <a href="">Editar Perfil</a><b>
+            <a href="">Rifas Atuais</a><br>
+            <a href="">Logout</a><br>
         </div>
     </div>
 
