ac6f6505c142852b75aa6de2482640b3edd358e3
alícia <103116333+oThisgo@users.noreply.github.com>
2022-05-30 09:19:45 -0300

    30.05 FINAL

---
+++ admin/rifas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    
+<div id="logo">
+<img src="https://megarifaonline.com.br/wp-content/uploads/2020/11/logotipo_mega_rifa_site.png" style="height: 80px; margin-left: 20px;">
+</div>
+
+<table border="5">
+    <tr>
+        <td><b>RIFAS</b></td>
+        <td><B>PRÊMIO 1</B></td>
+        <td><b>PRÊMIO 2</b></td>
+        <td><b>PRÊMIO 3</b></td>
+    </tr>
+</table>
+
+</body>
+</html>
---
+++ admin/users.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style2.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    
+<div id="logo">
+<img src="https://megarifaonline.com.br/wp-content/uploads/2020/11/logotipo_mega_rifa_site.png" style="height: 80px; margin-left: 20px;">
+</div>
+
+<table border="5">
+    <tr>
+        <td><b>USUÁRIO</b></td>
+        <td><B>NOME</B></td>
+        <td><b>SOBRENOME</b></td>
+        <td><b>EMAIL</b></td>
+        <td><b>SENHA</b></td>
+    </tr>
+</table>
+
+</body>
+</html>
